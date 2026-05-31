5f60d3f97e924b53bfe59298e0fd43e4a08ce749
szywat <szywat04@gmail.com>
2026-05-31 20:24:42 +0200

    Caching data with redis

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -5,9 +5,19 @@ const { pool } = require("../db");
 // GET /api/tasks
 router.get("/", async (req, res) => {
   try {
+    const cachedTasks = await req.redis.get("all_tasks");
+
+    if (cachedTasks) {
+      console.log("Pobrano z Redisa");
+      return res.json(JSON.parse(cachedTasks));
+    }
+
     const { rows } = await pool.query(
       "SELECT * FROM tasks ORDER BY created_at DESC",
     );
+
+    await req.redis.setEx("all_tasks", 60, JSON.stringify(rows));
+
     res.json(rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -23,6 +33,9 @@ router.post("/", async (req, res) => {
       "INSERT INTO tasks (title) VALUES ($1) RETURNING *",
       [title],
     );
+
+    await req.redis.del("all_tasks");
+
     res.status(201).json(rows[0]);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -39,6 +52,9 @@ router.patch("/:id", async (req, res) => {
     );
     if (rows.length === 0)
       return res.status(404).json({ error: "Nie znaleziono" });
+
+    await req.redis.del("all_tasks");
+
     res.json(rows[0]);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -49,7 +65,12 @@ router.patch("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    await pool.query("DELETE FROM tasks WHERE id = $1", [id]);
+    const { rows } = await pool.query("DELETE FROM tasks WHERE id = $1", [id]);
+
+    if (rows === 0) return res.status(404).json({ error: "Nie znaleziono" });
+
+    await req.redis.del("all_tasks");
+
     res.status(204).send();
   } catch (err) {
     res.status(500).json({ error: err.message });
