a073416181781ce3e24bd40ded330e6b43cec419
szywat <szywat04@gmail.com>
2026-05-31 15:16:37 +0200

    Changed port to 5000

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const { initDB } = require("./db");
 const tasksRouter = require("./routes/tasks");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
